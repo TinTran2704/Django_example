6a8061a62fbfc9c7ef2a7d43c70dfb8ec3006633
TinTran2704 <tranbaotinpro@gmail.com>
2022-04-21 15:31:22 +0700

    manage_commit_third_commit

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crm1.settings') #Define the environment variable to configured
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tin_sever.settings') #Define the environment variable to configured
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
