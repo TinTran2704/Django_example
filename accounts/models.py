af18821e5e988c36ad9354f88a3ca32f308198a5
TinTran2704 <tranbaotinpro@gmail.com>
2022-04-24 09:59:58 +0700

    update html and decorators

--- accounts/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class Customer(models.Model):
-    name = models.CharField(max_length=200, null = True)
-    phone = models.CharField(max_length=200)
-    email = models.CharField(max_length=200)
-    date_created = models.DateTimeField(auto_now_add = True)
